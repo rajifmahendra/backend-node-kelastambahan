8321aa74397977ff264eb6852dd7952ae29cffe4
rajifmahendra10 <rajifmahendra10@gmail.com>
2022-12-09 16:50:12 +0700

    update

--- routes/rentals.js
+++ routes/rentals.js
@@ -7,7 +7,7 @@ const Fawn = require("fawn");
 const express = require("express");
 const router = express.Router();
 
-Fawn.init(mongoose);
+Fawn.init("mongodb://127.0.0.1/backend-kelas");
 
 router.get("/", auth, async (req, res) => {
   const rentals = await Rental.find()
